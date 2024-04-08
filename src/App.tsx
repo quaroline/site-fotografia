65078891076549f560b9cd237c0de26ca72f77a9
Carolynne Padilha da Silva <carol-akiyama@hotmail.com>
2024-04-08 14:45:51 -0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Carousel from './carousel/Carousel'
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import ScrollToTopButton from './utils/scroll-to-top-button/ScrollToTopButton'
+import WhatsappButton from './utils/whatsapp-button/WhatsappButton'
 
 const App = () => {
   return (
     <>
+      <Header></Header>
       <Carousel></Carousel>
+      <WhatsappButton></WhatsappButton>
+      <ScrollToTopButton></ScrollToTopButton>
+      <Footer></Footer>
     </>
   )
 }
